dc0d4397e92fd456be0095d4b1ecc8b50e79f6f3
Rahul Serodia <Rahul.Serodia@coxautoinc.com>
2025-05-30 17:21:57 +0530

    Added landing page and social signup page UI only

---
+++ Splitshare/View/Login/LandingView.swift
@@ -0,0 +1,45 @@
+//
+//  LandingView.swift
+//  Splitshare
+//
+//  Created by Rahul Serodia on 25/05/25.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        ZStack {
+            Image("welcome_screen")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+
+                LoginButton(title: "Get Started") {
+                    
+                }
+                .padding(.bottom)
+                
+                LoginButton(title: "I Already Have An Account", type: .secondary) {
+                    
+                    
+                }
+                .padding(.bottom, 20)
+                
+                Image("appLogo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: .screenWidth, height: 150)
+                    .padding(.bottom, 0)
+
+            }
+        }
+    }
+}
+
+#Preview {
+    LandingView()
+}
